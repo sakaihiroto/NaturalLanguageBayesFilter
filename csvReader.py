e3310690f5132d099d9c2f35fec87f2f6ab207fc
sakai hiroto <mrkozou@gmail.com>
2017-10-27 21:33:12 +0900

    csvRaderDefCrate

--- csvReader.py
+++ csvReader.py
@@ -1,6 +1,14 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import os
+from docutils.parsers.rst.directives import encoding
 
-import panda as import pd
+def reader():
+    os.chdir("./csv")
+    df = pd.read_csv('sample.csv',encoding='utf-8',engine='python')
+    
+    return df
 
-def reader(self):
+if __name__ == '__main__':
+    reader()
 
-    df =
