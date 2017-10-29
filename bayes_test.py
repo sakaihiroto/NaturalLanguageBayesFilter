606cf4188e2d3e77867682d6064a55eca172c015
sakai hiroto <mrkozou@gmail.com>
2017-10-29 16:19:54 +0900

    AddignorFilee

--- bayes_test.py
+++ bayes_test.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
+import pickle
+
 from bayes import BayesianFilter
 import csvReader
 
-bf = BayesianFilter()
 
-df = csvReader.reader()
+@profile
+def study():
+    bf = BayesianFilter()
 
-columns = df.columns
+    dfLists = csvReader.reader()
 
-for col in columns:
-    print(col)
-    
-    for val in df[col]:
-        print(val)
+    for df in dfLists:
+        columns = df.columns
 
-        bf.fit(val,col)
+        for col in columns:
+            print(col)
+
+            for val in df[col]:
+                print(val)
+
+                bf.fit(val, col)
+
+    pre, scorelist = bf.predict("兵庫県川西市坂根二丁目25-6-305")
+
+    print("結果=", pre)
+    print(scorelist)
 
 
-pre, scorelist = bf.predict("兵庫県川西市坂根二丁目25-6-305")
-
-print("結果=", pre)
-print(scorelist)
-
+if __name__ == '__main__':
+    study()
--- csvReader.py
+++ csvReader.py
@@ -3,11 +3,18 @@ import pandas as pd
 import os
 from docutils.parsers.rst.directives import encoding
 
+dfLists=[]
+
 def reader():
-    os.chdir("./csv")
-    df = pd.read_csv('sample.csv',encoding='utf-8',engine='python')
+    csvFileNameLists = os.listdir('./csv')
+    os.chdir('./csv')
     
-    return df
+    for fileName in csvFileNameLists:
+        df = pd.read_csv(fileName, encoding='utf-8', engine='python')
+        
+        dfLists.append(df)
+
+    return dfLists
 
 if __name__ == '__main__':
     reader()
